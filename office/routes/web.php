1e52e59d749aedb48376c9ea2f9e88a17f6e582d
OneBigOwnage <niekvandenbos@hotmail.com>
2019-04-27 18:49:08 +0200

    Create a basic dashboard page.

--- office/routes/web.php
+++ office/routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('/dashboard', 'dashboard');
+
 Route::get('/ships', 'ShipController@index');
 Route::get('/concepts', 'TrainingConceptController@index');
 Route::get('/trainings', 'TrainingController@index');
