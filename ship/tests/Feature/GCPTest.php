67b61e84f360239bfa7a98f09df0f3cc21a0837f
Niek <niekvandenbos@hotmail.com>
2019-06-06 14:48:35 +0200

    Add a test to the GCPTest, to insert records.

--- ship/tests/Feature/GCPTest.php
+++ ship/tests/Feature/GCPTest.php
@@ -10,19 +10,29 @@ class GCPTest extends TestCase
     /** @test */
     public function it_can_retrieve_values_from_google_cloud_platform()
     {
-        $config = [
-            'keyFilePath' => resource_path('GCPKey.json'),
-            'projectId' => 'wise-vim-242413'
-        ];
-
         $dataSetID = 'test_dataset';
         $tableID   = 'test_table';
 
-        $client = new BigQueryClient($config);
+        $client = app()->make(BigQueryClient::class);
 
         $tableRows = $client->dataset($dataSetID)->table($tableID)->rows();
 
         $this->assertEquals('Niek', $tableRows->current()['name']);
         $this->assertEquals(23    , $tableRows->current()['age']);
+    }
+
+    /** @test */
+    public function it_can_send_data_to_big_query()
+    {
+        $client = app()->make(BigQueryClient::class);
+
+        $result = $client->dataset('test_dataset')->table('operating_hours_test')->insertRow([
+            'device'       => 'Test device',
+            'hours'        => mt_rand(2500, 15000)    ,
+            'reading_date' => '2019-06' ,
+        ]);
+
+        dump($result);
+
     }
 }
